03f8fc6467e69c53890cb77340d557e5351a05af
edenpaulocruz <edenpaulo@gmail.com>
2021-04-16 19:50:42 -0300

    Criando uma rota para listar contas

--- main.go
+++ main.go
@@ -1,9 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
+
+type Account struct {
+	Id         int
+	Name       string
+	Cpf        int
+	Secret     int
+	Balance    float64
+	Created_at time.Time
+}
+
+var Accounts []Account = []Account{
+	{
+		Id:         1,
+		Name:       "Fulano de Tal",
+		Cpf:        11122233344,
+		Secret:     552213,
+		Balance:    1000,
+		Created_at: time.Date(2021, time.April, 16, 12, 0, 0, 0, time.UTC),
+	},
+	{
+		Id:         2,
+		Name:       "Siclano de Tal",
+		Cpf:        22211133344,
+		Secret:     225513,
+		Balance:    1500,
+		Created_at: time.Date(2021, time.April, 16, 13, 0, 0, 0, time.UTC),
+	},
+	{
+		Id:         3,
+		Name:       "Beltrano de Tal",
+		Cpf:        33311122244,
+		Secret:     135522,
+		Balance:    0,
+		Created_at: time.Date(2021, time.April, 16, 19, 0, 0, 0, time.UTC),
+	},
+}
 
 func serverConfig() {
 	routesConfig()
@@ -14,10 +52,16 @@ func serverConfig() {
 
 func routesConfig() {
 	http.HandleFunc("/", root)
+	http.HandleFunc("/accounts", accountsList)
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Olá Stone!")
+}
+
+func accountsList(w http.ResponseWriter, r *http.Request) {
+	encoder := json.NewEncoder(w)
+	encoder.Encode(Accounts)
 }
 
 func main() {
